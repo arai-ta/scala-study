b6ed933c529e1fcfd344b0a49d923da07ff9bf6e
Arai Takashi <takashi.arai.nz@icloud.com>
2017-10-19 06:14:49 +0900

    test no type conversion (Int to Double)

--- Distance.scala
+++ Distance.scala
@@ -1,6 +1,6 @@
 object Distance extends App {
-  val kilometersPerHours = 11
-  val minutes = 100
+  val kilometersPerHours = 11.0
+  val minutes = 100.0
   val distance = kilometersPerHours * minutes / 60
   println(s"走った距離は${distance}キロメートルです")
 }
